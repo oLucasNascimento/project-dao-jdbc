2d174e543228528ce01f44cc3df1d100bdf4044d
oLucasNascimento <lucas.nferreira@bandtec.com.br>
2022-05-01 16:26:24 -0300

    SellerDaoJDBC and DaoFactory created

---
+++ src/main/java/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,34 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+    
+    @Override
+    public void insert(Seller seller) {
+    
+    }
+    
+    @Override
+    public void update(Seller seller) {
+    
+    }
+    
+    @Override
+    public void deleteById(Integer id) {
+    
+    }
+    
+    @Override
+    public void findById(Integer id) {
+    
+    }
+    
+    @Override
+    public void findAll(List<Seller> findAll) {
+    
+    }
+}
